9e33885c59a835015cdd7d6c68e8d1512b81e055
asus <de>
2020-10-02 14:59:54 +0300

    added file SauceDemoLocators

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.CartPage;
+import pages.CheckoutPage;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    WebDriver driver;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+    CartPage cartPage;
+    CheckoutPage checkoutPage;
+    public static final String USERNAME = "standard_user";
+    public static final String PASSWORD = "secret_sauce";
+    public static final String FIRST_NAME = "Kate";
+    public static final String LAST_NAME = "Doll";
+    public static final String POST_CODE = "200586 kl";
+
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resourses/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutPage = new CheckoutPage(driver);
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
